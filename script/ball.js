61220adae0b3df32822d39b35164b3a3555437a4
Nguyen Khac Nhat <khacnhat.info@gmail.com>
2019-05-24 18:44:48 +0700

    First commit

---
+++ script/ball.js
@@ -0,0 +1,169 @@
+let Ball = function (x, y, radius, color, xSpeed, ySpeed, imgId) {
+    this.imgId = imgId || rockRadId(7);
+    this.isSpin = true;
+    this.isExist = true;
+    this.x = x || canvas.width / 2;
+    this.y = y || canvas.height / 2;
+    this.color = color || rainbow(Math.random());
+    this.radius = radius || radNum(60, 60);
+    this.xSpeed = xSpeed || radNum(5, 0);
+    this.ySpeed = ySpeed || radNum(5, 0);
+    this.angle = 0;
+    this.damage = this.radius / 4;
+
+
+    this.increaseAngle = function (n) {
+        if (this.isSpin) {
+            this.angle = this.angle + n;
+            if (this.angle >= 360) {
+                this.angle = 0;
+            }
+        }
+    };
+    this.makeAMove = function () {
+        this.x += this.xSpeed;
+        this.y += this.ySpeed;
+        this.increaseAngle(sqrtOf2Sqr(this.xSpeed, this.ySpeed) / 3);
+    };
+
+    this.relocale = function (x, y, distance) {
+        let maxDistance = this.radius + distance;
+        let realDistance = sqrtOf2Sqr(this.x - x, this.y - y);
+        if (maxDistance > realDistance) {
+            this.x = (this.x - x) * (maxDistance / realDistance) + x;
+            this.y = (this.y - y) * (maxDistance / realDistance) + y;
+        }
+    };
+    this.toEdge = function () {
+        if (this.x <= this.radius) {
+            this.x = this.radius;
+            this.xSpeed *= -1;
+
+        }
+        else if (this.x >= (canvas.width - this.radius)) {
+            this.x = canvas.width - this.radius;
+            this.xSpeed *= -1;
+
+        } else if (this.y <= this.radius) {
+            this.y = this.radius;
+            this.ySpeed *= -1;
+        } else if (this.y >= (canvas.height - this.radius)) {
+            this.y = canvas.height - this.radius;
+            this.ySpeed *= -1;
+        }
+    };
+    this.toBall = function (otherBall) {
+        for (let ball of otherBall) {
+            let ballMaxDistance = ball.radius + this.radius;
+            let ballRealDistance = sqrtOf2Sqr(ball.x - this.x, ball.y - this.y);
+            if (ballRealDistance !== 0) {
+                if (ballMaxDistance > ballRealDistance) {
+                    this.relocale(ball.x, ball.y, ball.radius);
+                }
+                if (ballMaxDistance >= ballRealDistance) {
+                    let thisXSpeedAfter =
+                        ((this.radius - ball.radius) * this.xSpeed
+                            + 2 * ball.radius * ball.xSpeed) / ballMaxDistance;
+                    let thisYSpeedAfter =
+                        ((this.radius - ball.radius) * this.ySpeed
+                            + 2 * ball.radius * ball.ySpeed) / ballMaxDistance;
+                    let ballXSpeedAfter =
+                        ((ball.radius - this.radius) * ball.xSpeed
+                            + 2 * this.radius * this.xSpeed) / ballMaxDistance;
+                    let ballYSpeedAfter =
+                        ((ball.radius - this.radius) * ball.ySpeed
+                            + 2 * this.radius * this.ySpeed) / ballMaxDistance;
+                    this.xSpeed = thisXSpeedAfter;
+                    this.ySpeed = thisYSpeedAfter;
+                    ball.xSpeed = ballXSpeedAfter;
+                    ball.ySpeed = ballYSpeedAfter;
+
+                }
+
+            }
+        }
+    };
+    this.toObj = function (obj) {
+
+        let ballMaxDistance = this.radius + obj.radius;
+        let ballRealDistance = sqrtOf2Sqr(this.x - obj.x, this.y - obj.y);
+        if (ballRealDistance !== 0) {
+            if (ballRealDistance < ballMaxDistance) {
+                this.relocale(obj.x, obj.y, obj.radius);
+            }
+            if (ballRealDistance <= ballMaxDistance) {
+                this.xSpeed *= -1;
+                this.ySpeed *= -1;
+                return true;
+            }
+        }
+
+    };
+    this.toArrOfObj = function (otherBall) {
+        for (let i = 0; i < otherBall.length; i++) {
+            let ballMaxDistance = otherBall[i].radius + this.radius;
+            let ballRealDistance = sqrtOf2Sqr(otherBall[i].x - this.x, otherBall[i].y - this.y);
+            if (ballMaxDistance >= ballRealDistance) {
+                return i;
+            }
+        }
+        return -1;
+    };
+
+
+    this.remove = function (balls) {
+        this.isExist = false;
+        for (let i = 0; i < balls.length; i++) {
+            if (balls[i].isExist) {
+            }
+            else {
+                balls.splice(i, 1);
+            }
+        }
+    };
+    this.explore = function (balls, n = 1) {
+        this.remove(balls);
+        if (this.radius / 2 > 10) {
+            for (let j = 0; j < n; j++) {
+                balls.push(new Ball(this.x, this.y, this.radius / 1.5, this.color, undefined, undefined, this.imgId));
+            }
+
+        }
+    };
+    this.spawn = function (balls, n = 1) {
+        for (let i = 0; i < n; i++) {
+            balls.push(new Ball(this.x, this.y, undefined, this.color))
+        }
+    };
+    this.makeExplosive = function (containerArr, time) {
+        if (radNum(1, 0)) {
+            containerArr.push(new Explosiveball(this.x, this.y, this.radius * 2, "explosive0", time));
+            containerArr.push(new Explosiveball(this.x, this.y, this.radius * 1.2, undefined, time));
+        }
+        else if (radNum(1,0)) {
+            containerArr.push(new Explosiveball(this.x, this.y, this.radius * 2, undefined, time));
+        }
+        else {
+            containerArr.push(new Explosiveball(this.x, this.y, this.radius * 3.5, "explosive4", time));
+            containerArr.push(new Explosiveball(this.x, this.y, this.radius * 1, undefined, time));
+        }
+    };
+
+};
+
+let Explosiveball = function (x, y, radius, imgId, time) {
+    this.super = Ball;
+    this.super.call(this, x, y, radius, "yellow", 0.2, 0.2);
+    this.imgId = imgId || explosiveRadId(4);
+    this.count = time * 50 || 50;
+
+    this.drawExplosive = function (containerArr) {
+        if (this.count > 0) {
+            drawImgInBall(this, false);
+            this.count -= 1;
+        }
+        else {
+            this.remove(containerArr);
+        }
+    }
+};
---
+++ script/game.js
@@ -0,0 +1,286 @@
+let Game = function () {
+    this.score = 0;
+    this.items = [];
+    this.ship = new Ship(200, 800, 5, 100, "white");
+    this.spawner = new Ball(undefined, undefined, 100, undefined, 0, 0, "planet");
+    this.balls = [];
+    this.ammos = [];
+    this.spammos = [];
+    this.explosive = [];
+    this.itemstate = function () {
+        for (let item of this.items) {
+            item.toBall(this.items);
+            item.toObj(this.spawner);
+            item.toEdge();
+            if (item.toObj(this.ship)) {
+                if (item.imgId === "item4") {
+                    this.score += 30;
+                }
+                item.doFunc(this.ship);
+                item.remove(this.items);
+            }
+            item.makeAMove();
+            drawImgInBall(item);
+        }
+    };
+    this.spammostate = function () {
+        for (let ammo of this.spammos) {
+            if (ammo.toObj(this.ship) && ammo.imgId === "ammo1") {
+                this.ship.health -= ammo.damage;
+            }
+            if (ammo.imgId === "ammo3") {
+                for (let ball of this.balls) {
+                    ball.makeExplosive(this.explosive)
+                }
+                for (let ammo of this.ammos) {
+                    this.explosive.push(new Explosiveball(ammo.x, ammo.y, ammo.radius / 2, "explosive5", 0.3));
+                }
+                this.balls = [];
+                this.ammos = [];
+                this.spammos=[];
+            }
+            if (ammo.imgId === "ammo2") {
+                ammo.toEdge();
+                ammo.toObj(this.spawner);
+                if (ammo.toObj(this.ship)) {
+                    this.ship.health += 3;
+                }
+            }
+            ammo.makeAMove();
+            drawImgInBall(ammo);
+
+        }
+    };
+
+    this.ammostate = function () {
+        for (let ammo of this.ammos) {
+            ammo.toEdge();
+            ammo.toObj(this.spawner);
+            if (ammo.toObj(this.ship)) {
+                this.ship.health -= ammo.damage;
+            }
+            if (ammo.toArrOfObj(this.balls) >= 0) {
+                let exball = this.balls[ammo.toArrOfObj(this.balls)];
+                if (radNum(1, 0)) {
+                    if (exball.radius / 2 > 10) {
+                        exball.explore(this.balls, 2);
+                        exball.makeExplosive(this.explosive);
+                        ammo.remove(this.ammos);
+                        this.score += 5;
+                    }
+                    else {
+                        if (radNum(4, 0)) {
+                        } else {
+                            this.items.push(new Item(exball.x, exball.y));
+                        }
+                        if (radNum(5, 0)) {
+                        } else {
+                            this.items.push(new Item(exball.x, exball.y, "item1"));
+                        }
+                        if (radNum(18, 0)) {
+                        } else {
+                            this.items.push(new Item(exball.x, exball.y, "item2"));
+                        }
+                        if (radNum(20, 0)) {
+                        } else {
+                            this.items.push(new Item(exball.x, exball.y, "item3"));
+                        }
+                        if (radNum(3, 0)) {
+                        } else {
+                            this.items.push(new Item(exball.x, exball.y, "item4"));
+                        }
+                        exball.remove(this.balls);
+                        exball.makeExplosive(this.explosive, 0.3);
+                        this.score += 5;
+                    }
+                } else {
+                    ammo.toBall(this.balls);
+                }
+            }
+            ammo.makeAMove();
+        }
+    };
+
+    this.ballstate = function () {
+        for (let ball of this.balls) {
+            ball.toEdge();
+            ball.toObj(this.spawner);
+            if (ball.toObj(this.ship)) {
+                ball.explore(this.balls, 2);
+                this.ship.health -= ball.damage;
+                this.score -= 10;
+                ball.makeExplosive(this.explosive);
+            }
+            if (ball.toArrOfObj(this.ammos) >= 0) {
+                if (radNum(1, 0)) {
+                    this.ammos[ball.toArrOfObj(this.ammos)].remove(this.ammos);
+                    ball.makeExplosive(this.explosive);
+                    if (ball.radius / 2 > 10) {
+                        ball.explore(this.balls, 2);
+                        this.score += 5;
+                    } else {
+                        if (radNum(4, 0)) {
+                        } else {
+                            this.items.push(new Item(ball.x, ball.y));
+                        }
+                        if (radNum(5, 0)) {
+                        } else {
+                            this.items.push(new Item(ball.x, ball.y, "item1"));
+                        }
+                        if (radNum(18, 0)) {
+                        } else {
+                            this.items.push(new Item(ball.x, ball.y, "item2"));
+                        }
+                        if (radNum(25, 0)) {
+                        } else {
+                            this.items.push(new Item(ball.x, ball.y, "item3"));
+                        }
+                        if (radNum(3, 0)) {
+                        } else {
+                            this.items.push(new Item(ball.x, ball.y, "item4"));
+                        }
+                        ball.remove(this.balls);
+                        ball.makeExplosive(this.explosive, 0.3);
+                        this.score += 5;
+                    }
+                } else {
+                    ball.toBall(this.ammos);
+                }
+            }
+            if (ball.toArrOfObj(this.spammos) >= 0) {
+                if (radNum(1, 0)) {
+                    ball.remove(this.balls);
+                } else {
+                    if (ball.radius / 2 > 15) {
+                        ball.explore(this.balls, 2);
+                    } else {
+                        ball.remove(this.balls);
+                    }
+                }
+                ball.makeExplosive(this.explosive);
+                this.score += 5;
+            }
+            ball.toBall(this.balls);
+            ball.makeAMove();
+        }
+    };
+    this.drawBall = function () {
+        for (let ball of this.balls) {
+            drawImgInBall(ball)
+        }
+    };
+    this.drawAmmo = function () {
+        for (let ammo of this.ammos) {
+            drawImgInBall(ammo, true)
+        }
+    };
+    this.shipstate = function () {
+        if (this.ship.health <= 0) {
+            canvasClean();
+            drawBackGround();
+            setTimeout(function () {
+                if (confirm("play again?")) {
+                    window.location.reload();
+                }
+            }, 1000)
+
+        }
+        this.ship.toObj(this.spawner);
+        this.ship.toEdge();
+        this.ship.makeAMove();
+    };
+
+    this.displayscore = function (x, y, string = "") {
+        ctx.font = "25px Verdana";
+        ctx.fillStyle = "white";
+        ctx.fillText(string, x, y - 30);
+        ctx.fillText("Health " + Math.floor(this.ship.health), x, y);
+        ctx.fillText("Items: " + this.ship.totalAmmo.length, x, y + 30);
+        ctx.fillText("Score " + this.score, x, y + 80);
+    };
+    this.drawExplosion = function () {
+        for (let e of this.explosive) {
+            e.drawExplosive(this.explosive);
+        }
+    };
+    this.combineState = function () {
+        this.spammostate();
+        this.ammostate();
+        this.ballstate();
+        this.shipstate();
+        this.itemstate();
+    }
+
+};
+let game = new Game();
+let n = 0;
+rockGenerator(7);
+ammoGenerator(3);
+itemsGenerator(4);
+explosiveGenerator(5);
+
+score();
+gameplay();
+harder();
+spawnBalls();
+
+function gameplay() {
+    canvasClean();
+    drawBackGround();
+    game.combineState();
+    game.drawExplosion();
+    game.drawAmmo();
+    game.drawBall();
+    drawImgInBall(game.spawner, true);
+    drawImgInBall(game.ship, true);
+    game.displayscore(15, 60, "stage: " + n);
+    if (game.ship.health > 0) {
+        setTimeout(gameplay, 20);
+    }
+    else {
+        drawImgInBall(game.ship, false, "explosive1")
+    }
+}
+
+function harder() {
+    n++;
+    setTimeout(harder, 90000)
+}
+
+function spawnBalls() {
+    game.spawner.spawn(game.balls, n);
+    game.spawner.color = rainbow(Math.random());
+    setTimeout(spawnBalls, 15000);
+}
+
+function score() {
+    game.score += n + Math.floor((game.balls.length + game.ammos.length) / 10);
+    setTimeout(score, 5000);
+}
+
+window.addEventListener("keydown", function (evt) {
+    switch (evt.key) {
+        case "ArrowRight":
+            game.ship.moveRight();
+            break;
+        case "ArrowLeft":
+            game.ship.moveLeft();
+            break;
+        case"ArrowUp":
+            game.ship.moveUp();
+            break;
+        case "ArrowDown":
+            game.ship.moveDown();
+            break;
+        case "a":
+            if (game.ship.totalAmmo.length > 0) {
+                let ammo = game.ship.totalAmmo.pop();
+                game.ship.shoot(game.spammos, this.angle, ammo.imgId, 10);
+            }
+            break;
+        case "s":
+            game.ship.shoot(game.ammos, this.angle, "ammo0");
+            break;
+    }
+});
+
